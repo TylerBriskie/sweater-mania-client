91fa891c3025bdd453b49d038e1073f802321411
Tyler Briskie <tylerbriskie@gmail.com>
2016-12-16 15:11:07 -0700

    working on get request promise chain

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-// const API_URL = getHostUrl();
+const API_URL = getHostUrl();
 
 $(document).ready(function(){
 
- let queryString = parseQuery(window.location.search);
-
-console.log(queryString);
+ let user_id = parseQuery(window.location.search);
+ // GET REQUEST
+ findUser(user_id)
 
 });
+
+//===================
+//-----functions-----
+//===================
+
+
+function getHostUrl(){
+  if (window.location.host.indexOf('localhost') != -1) {
+    return 'localhost:3000';
+  } else {
+    return 'https://sweater-mania.herokuapp.com/';
+  }
+};
 
 function parseQuery(query) {
 return query.split('=')[1];
 };
+
+
+
+function findUser(id){
+  return $.get(`${API_URL}/users/${id}`)
+
+}
+function addUserInfoToPage(user){
+  
+}
+
+function displaySweaters(data) {
+
+}
